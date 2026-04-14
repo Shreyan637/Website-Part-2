1d57357d2e158e5f4f1e56d3c813976b358c1e19
Shreyan637 <mugdhashreyan@gmail.com>
2026-04-14 22:54:14 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let slides =document.querySelectorAll(".slide");
-let index=0;
+// Simple slider
+let slides = document.querySelectorAll(".slide");
+let index = 0;
 
-function showNextSlide (){
-    slides[index].classList.remove("acitve");
-    index=(index+1) % slides.length;
-    slides[index].classList.add("active");
+function showNextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
 }
 setInterval(showNextSlide, 3000);
 
-const menuToggle = document.querySelector("menu-toggle");
+// Responsive menu toggle
+const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click",() => {
-    navLinks.classList.toggle("show");
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
